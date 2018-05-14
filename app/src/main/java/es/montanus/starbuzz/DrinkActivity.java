ebfd7a4e43f9cfee45fb94361388bacded7ddaa9
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-14 16:43:35 +0200

    DrinkActivity

--- app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
+++ app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
@@ -2,6 +2,7 @@ package es.montanus.starbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
@@ -10,9 +11,34 @@ public class DrinkActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+        initViews();
+    }
 
+    private void initViews() {
+        Drink drink = getDrink();
+        setName(drink.getName());
+        setDescription(drink.getDescription());
+        setImage(drink.getImageResourceId(), drink.getName());
+    }
+
+    private void setName(String name) {
+        TextView nameView = findViewById(R.id.name);
+        nameView.setText(name);
+    }
+
+    private void setDescription(String description) {
+        TextView descriptionView = findViewById(R.id.description);
+        descriptionView.setText(description);
+    }
+
+    private void setImage(int imageResourceId, String contentDescription) {
+        ImageView photo = findViewById(R.id.photo);
+        photo.setImageResource(imageResourceId);
+        photo.setContentDescription(contentDescription);
+    }
+
+    private Drink getDrink() {
         long id = getIntent().getLongExtra(DrinkCategoryActivity.DRINK_ID, -1);
-        TextView textId = (TextView)findViewById(R.id.text_id);
-        textId.setText(String.valueOf(id));
+        return Drink.drinks[(int)id];
     }
 }
