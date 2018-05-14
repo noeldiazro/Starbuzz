873317b35dfd894bbc2bbea78e9136d1cca76d0c
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-14 10:38:46 +0200

    TopLevelActivity

---
+++ app/src/main/java/es/montanus/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,13 @@
+package es.montanus.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+    }
+}
--- app/src/main/java/es/montanus/starbuzz/TopLevelActivity.java
+++ app/src/main/java/es/montanus/starbuzz/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package es.montanus.starbuzz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends Activity {
 
@@ -9,5 +13,23 @@ public class TopLevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        initListView();
+    }
+
+    private void initListView() {
+        ListView listView = (ListView)findViewById(R.id.list_options);
+        listView.setOnItemClickListener(makeOnItemClickListener());
+    }
+
+    private AdapterView.OnItemClickListener makeOnItemClickListener() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    startActivity(new Intent(TopLevelActivity.this,
+                            DrinkCategoryActivity.class));
+                }
+            }
+        };
     }
 }
