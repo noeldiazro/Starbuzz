18a9937815e7d5c93bfe5bdb036d1bf5b078a021
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-28 09:48:34 +0200

    Create table

--- app/src/main/java/es/montanus/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/es/montanus/starbuzz/StarbuzzDatabaseHelper.java
@@ -1,5 +1,6 @@
 package es.montanus.starbuzz;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,11 +16,56 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        StarbuzzDatabaseCreator creator = new StarbuzzDatabaseCreator(db);
+        creator.createTable();
+        creator.populateTable();
     }
+
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private static class StarbuzzDatabaseCreator {
+
+        private final SQLiteDatabase db;
+
+        private StarbuzzDatabaseCreator(SQLiteDatabase db) {
+            this.db = db;
+        }
+
+        private void createTable() {
+            db.execSQL("CREATE TABLE DRINK ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "IMAGE_RESOURCE_ID INTEGER)");
+        }
+
+
+        private void populateTable() {
+            insert(new Drink("Latte",
+                    "A couple of espresso shots with steamed milk",
+                    R.drawable.latte));
+
+            insert(new Drink("Cappuccino",
+                    "Espresso, hot milk, and a steamed milk foam",
+                    R.drawable.cappuccino));
+
+            insert(new Drink("Filter",
+                    "Highest quality beans roasted and brewed fresh",
+                    R.drawable.filter));
+        }
+
+        private void insert(Drink drink) {
+            ContentValues latteValues = new ContentValues();
+            latteValues.put("NAME", drink.getName());
+            latteValues.put("DESCRIPTION", drink.getDescription());
+            latteValues.put("IMAGE_RESOURCE_ID", drink.getImageResourceId());
+            db.insert("DRINK", null, latteValues);
+        }
     }
 }
