5d11d5d5503599c27518ea644587dded81c4e07a
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-28 09:16:02 +0200

    Create database

---
+++ app/src/main/java/es/montanus/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,25 @@
+package es.montanus.starbuzz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz";
+    private static final int DB_VERSION = 1;
+
+    public StarbuzzDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
