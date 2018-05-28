c9825fee5e24fe63003c94a5a29751c83f2d7094
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-28 13:07:38 +0200

    Navigate to record and display drink

--- app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
+++ app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
@@ -1,10 +1,7 @@
 package es.montanus.starbuzz;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,35 +9,32 @@ import android.widget.Toast;
 
 public class DrinkActivity extends Activity {
 
+    private DrinkRepository repository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        repository = new DrinkRepository(this);
         initViews();
     }
 
     private void initViews() {
         long id = getIntent().getLongExtra(DrinkCategoryActivity.DRINK_ID, -1);
-
-        SQLiteOpenHelper startbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        Drink drink = null;
         try {
-            SQLiteDatabase db = startbuzzDatabaseHelper.getReadableDatabase();
-            Cursor cursor = db.query("DRINK",
-                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
-                    "_id = ?",
-                    new String[] {Long.toString(id)},
-                    null, null, null);
-
-            cursor.close();
+            drink = repository.get(id + 1);
         }
         catch (SQLiteException e) {
             Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
         }
 
-        Drink drink = Drink.drinks[(int) id];
-        setName(drink.getName());
-        setDescription(drink.getDescription());
-        setImage(drink.getImageResourceId(), drink.getName());
+        if (drink != null) {
+            setName(drink.getName());
+            setDescription(drink.getDescription());
+            setImage(drink.getImageResourceId(), drink.getName());
+        }
     }
 
     private void setName(String name) {
---
+++ app/src/main/java/es/montanus/starbuzz/DrinkRepository.java
@@ -0,0 +1,36 @@
+package es.montanus.starbuzz;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DrinkRepository {
+    private final Context context;
+
+    public DrinkRepository(Context context) {
+        this.context = context;
+    }
+
+    public Drink get(long id) {
+        Drink drink = null;
+
+        SQLiteOpenHelper startbuzzDatabaseHelper = new StarbuzzDatabaseHelper(context);
+        SQLiteDatabase db = startbuzzDatabaseHelper.getReadableDatabase();
+        Cursor cursor = db.query("DRINK",
+                new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                "_id = ?",
+                new String[]{Long.toString(id)},
+                null, null, null);
+
+        if (cursor.moveToFirst())
+            drink = new Drink(cursor.getString(0),
+                    cursor.getString(1),
+                    cursor.getInt(2));
+
+        cursor.close();
+        db.close();
+
+        return drink;
+    }
+}
