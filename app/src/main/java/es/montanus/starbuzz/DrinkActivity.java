d7c0d91641c095aa9c935d980bdc069909ee531a
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-28 12:20:02 +0200

    Database reference

--- app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
+++ app/src/main/java/es/montanus/starbuzz/DrinkActivity.java
@@ -1,9 +1,14 @@
 package es.montanus.starbuzz;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends Activity {
 
@@ -15,7 +20,17 @@ public class DrinkActivity extends Activity {
     }
 
     private void initViews() {
-        Drink drink = getDrink();
+        long id = getIntent().getLongExtra(DrinkCategoryActivity.DRINK_ID, -1);
+
+        SQLiteOpenHelper startbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = startbuzzDatabaseHelper.getReadableDatabase();
+        }
+        catch (SQLiteException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+
+        Drink drink = Drink.drinks[(int) id];
         setName(drink.getName());
         setDescription(drink.getDescription());
         setImage(drink.getImageResourceId(), drink.getName());
@@ -37,8 +52,4 @@ public class DrinkActivity extends Activity {
         photo.setContentDescription(contentDescription);
     }
 
-    private Drink getDrink() {
-        long id = getIntent().getLongExtra(DrinkCategoryActivity.DRINK_ID, -1);
-        return Drink.drinks[(int)id];
-    }
 }
